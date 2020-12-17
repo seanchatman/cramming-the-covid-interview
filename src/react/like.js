3f6415af381871fffc37931b16ba879986ddf1f8
Sean Chatman <xpointsh@gmail.com>
2020-12-17 10:53:58 -0800

    First pycharm commit.

---
+++ src/react/like.js
@@ -0,0 +1,28 @@
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+    const app = document.querySelector('#app')
+ReactDOM.render(e(() => {
+  return (
+      <button>Like</button>
+  )
+}), app);
+
